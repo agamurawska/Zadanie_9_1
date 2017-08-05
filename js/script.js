adf1169aa573d6d08287fb1956a80926ad576bb0
Agnieszka <aga.murawska@gmail.com>
2017-08-05 21:46:47 +0200

    function in js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿function getTriangleArea (a, h) {
+
+if ((a <= 0) || (h <= 0))
+    return ('nieprawidłowe dane');
+
+	return (a*h/2);
+};
+
+console.log(getTriangleArea(10,6));
+
+var triangle1Area = getTriangleArea(10,15),
+triangle2Area = getTriangleArea(10,6),
+triangle3Area = getTriangleArea(15,8);
